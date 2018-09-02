dbe4738e106bad34354515ff8ec671e283113b01
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-09-01 21:58:46 -0400

    Updates

--- cypress/integration/first.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-it('adds 2 todos', () => {
-  cy.visit('/')
-  cy.get('.new-todo')
-    .type('learn testing{enter}')
-    .type('be cool{enter}')
-  cy.get('.todo-list li')
-    .should('have.length', 2)
-})
